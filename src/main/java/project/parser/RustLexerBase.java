980a14f0ce8b1c78d2f16c36fe8757938f574580
Matt Port <mattp0@duck.com>
2026-05-17 20:54:40 +0100

    add .g4 files and base lexer/parser
    downloaded from grammars-v4 community antlr grammars

---
+++ src/main/java/project/parser/RustLexerBase.java
@@ -0,0 +1,119 @@
+/*
+Copyright (c) 2010 The Rust Project Developers
+Copyright (c) 2020-2022 Student Main
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice (including the next paragraph) shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package project.parser;
+import org.antlr.v4.runtime.*;
+
+public abstract class RustLexerBase extends Lexer{
+    public RustLexerBase(CharStream input){
+        super(input);
+    }
+
+    Token lt1;
+    Token lt2;
+
+    @Override
+    public Token nextToken() {
+        Token next = super.nextToken();
+
+        if (next.getChannel() == Token.DEFAULT_CHANNEL) {
+            // Keep track of the last token on the default channel.
+            this.lt2 = this.lt1;
+            this.lt1 = next;
+        }
+
+        return next;
+    }
+
+    public boolean SOF(){
+        return _input.LA(-1) <=0;
+    }
+    
+    public boolean FloatDotPossible(){
+        int next = _input.LA(1);
+        // only block . _ identifier after float
+        if(next == '.' || next =='_')
+        {
+            return false;
+        }
+        if(next == 'f') {
+            // 1.f32
+            if (_input.LA(2)=='3'&&_input.LA(3)=='2')
+            {
+                return true;
+            }
+            //1.f64
+            if (_input.LA(2)=='6'&&_input.LA(3)=='4')
+            {
+                return true;
+            }
+            return false;
+        }
+        if(next>='a'&&next<='z') {
+            return false;
+        }
+        if(next>='A'&&next<='Z') {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean FloatLiteralPossible(){
+        if(this.lt1 == null || this.lt2 == null)
+        {
+            return true;
+        }
+        if(this.lt1.getType() != RustLexer.DOT)
+        {
+            return true;
+        }
+        switch (this.lt2.getType()){
+            case RustLexer.CHAR_LITERAL:
+            case RustLexer.STRING_LITERAL:
+            case RustLexer.RAW_STRING_LITERAL:
+            case RustLexer.BYTE_LITERAL:
+            case RustLexer.BYTE_STRING_LITERAL:
+            case RustLexer.RAW_BYTE_STRING_LITERAL:
+            case RustLexer.INTEGER_LITERAL:
+            case RustLexer.DEC_LITERAL:
+            case RustLexer.HEX_LITERAL:
+            case RustLexer.OCT_LITERAL:
+            case RustLexer.BIN_LITERAL:
+
+            case RustLexer.KW_SUPER:
+            case RustLexer.KW_SELFVALUE:
+            case RustLexer.KW_SELFTYPE:
+            case RustLexer.KW_CRATE:
+            case RustLexer.KW_DOLLARCRATE:
+
+            case RustLexer.GT:
+            case RustLexer.RCURLYBRACE:
+            case RustLexer.RSQUAREBRACKET:
+            case RustLexer.RPAREN:
+
+            case RustLexer.KW_AWAIT:
+
+            case RustLexer.NON_KEYWORD_IDENTIFIER:
+            case RustLexer.RAW_IDENTIFIER:
+            case RustLexer.KW_MACRORULES:
+                return false;
+            default:
+                return true;
+        }
+    }
+}
