bc7ec415ac0344c28344f37f165f733fec9f35a7
Matt Port <mattp0@duck.com>
2026-06-04 00:21:12 +0100

    Translator start
    implement Lctrs record and Sort.of

--- src/main/java/project/lctrs/Sort.java
+++ src/main/java/project/lctrs/Sort.java
@@ -1,9 +1,18 @@
 package project.lctrs;
 
+import project.ast.Type;
+
 /** A sort in the many-sorted LCTRS signature. */
 public enum Sort {
   /** The integer sort. */
   INT,
   /** The boolean sort. */
-  BOOL
+  BOOL;
+
+  public static Sort of(Type type) {
+    return switch (type) {
+      case Type.Int _ -> INT;
+      case Type.Bool _ -> BOOL;
+    };
+  }
 }
