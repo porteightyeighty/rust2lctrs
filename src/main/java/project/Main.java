df647b4200ddf0d42e58dbb9d29795d5b95c26c9
Matt Port <mattp0@duck.com>
2026-06-08 18:51:34 +0100

    cli: add picocli entry point taking a Rust source file

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,32 +1,11 @@
 package project;
 
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.TokenStream;
-import project.ast.AstBuilder;
-import project.ast.Crate;
-import project.ast.SpanTable;
-import project.lctrs.Lctrs;
-import project.lctrs.Serialiser;
-import project.parser.RustLexer;
-import project.parser.RustParser;
-import project.translator.Translator;
+import picocli.CommandLine;
+import project.cli.Rust2LctrsCommand;
 
 public class Main {
-  static void main() {
-    String input = "fn main() -> i32 {let i: i32 = 0; let x: i32 = i + 5; return x;}";
-    CharStream inputStream = CharStreams.fromString(input);
-    RustLexer lexer = new RustLexer(inputStream);
-    TokenStream tokens = new CommonTokenStream(lexer);
-    RustParser parser = new RustParser(tokens);
-    RustParser.CrateContext crateCtx = parser.crate();
-    System.out.println(crateCtx.toStringTree(parser));
-    SpanTable spanTable = new SpanTable();
-    AstBuilder astBuilder = new AstBuilder(spanTable);
-    Crate crate = astBuilder.buildCrate(crateCtx);
-    Translator translator = new Translator(crate);
-    Lctrs lctrs = translator.translate();
-    System.out.println(Serialiser.serialise(lctrs));
+  public static void main(String[] args) {
+    int exitCode = new CommandLine(new Rust2LctrsCommand()).execute(args);
+    System.exit(exitCode);
   }
 }
